a0bc250d1be63c279844cfbeee0dcd2613fa3cd6
B1-Macbook <csiiosdev@gmail.com>
2018-09-03 14:22:35 +0700

    show info stubbing

--- run_mac.sh
+++ run_mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' > /tmp/a.o`
+`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head -n 1  > /tmp/a.o`
 ipaddress=`cat /tmp/a.o`
 echo ${ipaddress}
 java -jar stubby4j520.jar -d ${1} -l ${ipaddress} -w
